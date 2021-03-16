a77bcb6ba467e31409beb2c225d185b9113d6e23
zimlit <zimlit2010@gmail.com>
2021-03-16 19:30:33 -0400

    home

---
+++ app/javascript/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <navigation />
+    <h1 class="welcome">
+      Zimlit.com
+      <p>coding, cubing, conlanging, conworlding</p>
+    </h1>
+  </div>
+</template>
+
+<script>
+import navigation from './navigation'
+
+export default {
+
+  components: {
+    navigation
+  }
+}
+</script>
+
+<style scoped>
+  .welcome {
+    text-align: center;
+  }
+</style>
---
+++ app/javascript/navigation.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class=navbar>
+    <ul>
+      <li class="nav-item">
+        <a href="/coding">
+          coding
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="/cubing">
+          cubing
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="/conlanging">
+          conlanging
+        </a>
+      </li>
+
+      <li class="nav-item">
+        <a href="/conworlding">
+          conworlding
+        </a>
+      </li>
+    </ul>
+  </div>
+</template
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .navbar {
+    margin: 1em;
+    border-radius: 25px;
+    background: #555;
+  }
+
+  .nav-item {
+    margin: 0.5em 1em 0.5em 1em;
+    background: gray;
+    display: inline-block;
+    padding: 1em;
+    border-radius: 5px;
+  }
+
+  .nav-item:hover {
+    background: #777;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #369;
+  }
+
+  a:active {
+    text-decoration: underline;
+  }
+</style>
