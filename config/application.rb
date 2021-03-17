5cffdc264ed1f66c69d2288ddb981419fc59cda1
zimlit <zimlit2010@gmail.com>
2021-03-17 11:52:29 -0400

    redirect

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
+require 'rack-rewrite'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +11,9 @@ module ZimlitCom
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
+      r301 'zimlit-com-rails.herokuapp.com', 'www.zimlit.com'
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
